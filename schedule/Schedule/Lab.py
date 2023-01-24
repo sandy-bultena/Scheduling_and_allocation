49b6188a2b1c9d0d2267cf52f0c051edf4ee980d
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-01-24 11:53:04 -0500

    Backend: Expanding Lab.py. Need to shift gears to TimeSlot.py before I can continue.

--- schedule/Schedule/Lab.py
+++ schedule/Schedule/Lab.py
@@ -11,10 +11,10 @@ class Lab:
     """
     max_id = 0
 
-    def __init__(self, number: int = 100, desc: str = ''):
+    def __init__(self, number: int = 100, descr: str = ''):
         """Creates and returns a new Lab object."""
         self.__number = number
-        self.desc = desc
+        self.__descr = descr
         Lab.max_id += 1
         self.__id = Lab.max_id
 
@@ -29,3 +29,58 @@ class Lab:
     @number.setter
     def number(self, new_value):
         self.__number = new_value
+
+    @property
+    def descr(self):
+        return self.__descr
+
+    @descr.setter
+    def descr(self, new_value):
+        self.__descr = new_value
+
+    # =================================================
+    # add_unavailable
+    # =================================================
+    def add_unavailable(self):
+        """Creates a time slot where this lab is not available."""
+        # TODO: Come back to this when TimeSlot is defined.
+        pass
+
+    # =================================================
+    # add_unavailable
+    # =================================================
+    def remove_unavailable(self, id):
+        """Remove the unavailable time slot from this lab.
+        
+        Parameters:
+        ---
+        id: int
+            The id of the time slot to be removed.
+        """
+        # TODO: Come back to this when TimeSlot is defined.
+        pass
+
+    def get_unavailable(self, id):
+        """Return the unavailable time slot object for this Lab.
+        
+        Parameters:
+        ---
+        id: int
+            The id of the time slot to be returned.
+        
+        Returns:
+        ---
+        The TimeSlot object.
+        """
+        # TODO: Come back to this when TimeSlot is defined.
+        pass
+
+    def unavailable(self):
+        """Returns all unavailable time slot objects for this lab."""
+        pass
+
+    def print_description(self):
+        """Returns a text string that describes the Lab."""
+        if self.__descr is None:
+            return f"{self.__number}"
+        return f"{self.__number}: {self.__descr}"
