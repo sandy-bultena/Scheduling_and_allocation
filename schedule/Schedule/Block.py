e90eb1526245272991c36ca75cccdcfe21993f48
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-01-25 08:54:57 -0500

    Backend: Beginning to expand Block.py.
    Added synopsis & constructor.

---
+++ schedule/Schedule/Block.py
@@ -0,0 +1,43 @@
+from Time_slot import TimeSlot
+# SYNOPSIS:
+
+#    use Schedule::Course;
+#    use Schedule::Section;
+#    use Schedule::Block;
+    
+#    my $block = Block->new (-day=>"Wed",-start=>"9:30",-duration=>1.5);
+
+#    my $course = Course->new(-name=>"Basket Weaving");
+#    my $section = $course->create_section(-section_number=>1);
+#    $section->add_block($block);
+    
+#    print "block belongs to section ",$block->section;
+    
+#    $block->assign_teacher($teacher);
+#    $block->remove_teacher($teacher);
+#    $block->teachers();
+    
+#    $block->add_lab("P327");
+#    $block->remove_lab("P325");
+#    $block->labs();
+
+
+class Block(TimeSlot):
+    """
+    Describes a block which is a specific time slot for teaching part of a section of a course.
+    """
+
+    # =================================================================
+    # Class Variables
+    # =================================================================
+   
+    _max_id = 0
+    _default_day = 'mon'
+
+    # =================================================================
+    # Constructor
+    # =================================================================
+
+    def __init__(self, day: str, start: str, duration: float) -> None:
+        TimeSlot.__init__(self, day, start, duration)
+        pass
