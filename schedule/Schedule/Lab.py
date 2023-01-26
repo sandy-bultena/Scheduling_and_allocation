180f2ef8f5c2abe883d1ddb56016355f4fdc052b
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-01-26 11:17:31 -0500

    Backend: Expanding Lab.py.

--- schedule/Schedule/Lab.py
+++ schedule/Schedule/Lab.py
@@ -1,3 +1,5 @@
+from Time_slot import TimeSlot
+
 class Lab:
     """
     Describes a distinct, contiguous course/section/class.
@@ -9,18 +11,18 @@ class Lab:
     desc: str
         The description of the Lab.
     """
-    max_id = 0
+    _max_id = 0
 
     # -------------------------------------------------------------------
     # new
     # --------------------------------------------------------------------
 
-    def __init__(self, number: int = 100, descr: str = ''):
+    def __init__(self, number: str = "100", descr: str = ''):
         """Creates and returns a new Lab object."""
-        self.__number = number
-        self.__descr = descr
-        Lab.max_id += 1
-        self.__id = Lab.max_id
+        self.number = number
+        self.descr = descr
+        Lab._max_id += 1
+        self.__id = Lab._max_id
 
     # =================================================================
     # id
@@ -44,54 +46,85 @@ class Lab:
     def number(self, new_value):
         self.__number = new_value
 
+    # =================================================================
+    # descr
+    # =================================================================
+
     @property
     def descr(self):
+        """Sets/returns the description for this Lab object."""
         return self.__descr
 
     @descr.setter
-    def descr(self, new_value):
+    def descr(self, new_value: str):
         self.__descr = new_value
 
     # =================================================
     # add_unavailable
     # =================================================
-    def add_unavailable(self):
-        """Creates a time slot where this lab is not available."""
-        # TODO: Come back to this when TimeSlot is defined.
-        pass
+    def add_unavailable(self, day: str, start: str, duration: float):
+        """Creates a time slot where this lab is not available.
+        
+        - Parameter day => 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+
+        - Parameter start => start time using 24 h clock (i.e., 1pm is "13:00")
+
+        - Parameter duration => how long does this class last, in hours
+        """
+        if not hasattr(self, '_unavailable'):
+            self._unavailable: dict[int, TimeSlot] = {}
+
+        # Create a TimeSlot.
+        slot = TimeSlot(day, start, duration)
+
+        # Save it.
+        self._unavailable[slot.id] = slot
+
+        return self
 
     # =================================================
-    # add_unavailable
+    # remove_unavailable
     # =================================================
-    def remove_unavailable(self, id):
+    def remove_unavailable(self, target_id: int):
         """Remove the unavailable time slot from this lab.
         
-        Parameters:
-        ---
-        id: int
-            The id of the time slot to be removed.
-        """
-        # TODO: Come back to this when TimeSlot is defined.
-        pass
+        - Parameter target_id -> The ID of the time slot to be removed.
 
-    def get_unavailable(self, id):
+        Returns the modified Lab object.
+        """
+        if hasattr(self, '_unavailable') and target_id in self._unavailable.keys():
+            del self._unavailable[target_id]
+
+        return self
+
+    # =================================================================
+    # get_unavailable
+    # =================================================================
+
+    def get_unavailable(self, target_id: int):
         """Return the unavailable time slot object for this Lab.
         
-        Parameters:
-        ---
-        id: int
-            The id of the time slot to be returned.
-        
-        Returns:
-        ---
-        The TimeSlot object.
+        - Parameter target_id -> The ID of the TimeSlot to be returned.
+             
+        Returns the TimeSlot object.
         """
-        # TODO: Come back to this when TimeSlot is defined.
-        pass
+        if hasattr(self, '_unavailable') and target_id in self._unavailable.keys():
+            return self._unavailable[target_id]
+        return None
+
+    # =================================================================
+    # unavailable
+    # =================================================================
 
     def unavailable(self):
         """Returns all unavailable time slot objects for this lab."""
-        pass
+        if hasattr(self, '_unavailable'):
+            return list(self._unavailable.values())
+        return []
+
+    # =================================================================
+    # print_description
+    # =================================================================
 
     def print_description(self):
         """Returns a text string that describes the Lab."""
