ff6b7a99b9dea87977bf73aef95a03e2fc34b750
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-02-01 16:02:46 -0500

    Backend: Adding synopsis to Course.py.

--- schedule/Schedule/Course.py
+++ schedule/Schedule/Course.py
@@ -1,3 +1,20 @@
+# SYNOPSIS
+
+#    use Schedule::Course;
+    
+#    my $block = Block->new (-day=>"Wed",-start=>"9:30",-duration=>1.5);
+#    my $section = Section->new(-number=>1, -hours=>6);
+
+#    my $course = Course->new(-name=>"Basket Weaving", -course_id="420-ABC-DEF");
+#    $course->add_section($section);
+#    $section->add_block($block);
+    
+#    print "Course consists of the following sections: ";
+#    foreach my $section ($course->sections) {
+        # print info about $section
+#    }
+
+
 class Course:
     # ideally iterable is implemented with dict; if not, yaml write needs to be modified accordingly
     _max_id = 0
