efaf74f8a20b7719e936ab8acb1ef65eb432bc99
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-02-01 13:36:27 -0500

    Backend: Created pytest file for Lab class.

---
+++ schedule/Schedule/test_Lab.py
@@ -0,0 +1,43 @@
+from Lab import Lab
+
+
+def test_id():
+    """Verifies that the ID property automatically increments as Labs are created."""
+    lab = Lab()
+    assert lab.id == 1
+
+
+def test_number():
+    assert False
+
+
+def test_number():
+    assert False
+
+
+def test_descr():
+    assert False
+
+
+def test_descr():
+    assert False
+
+
+def test_add_unavailable():
+    assert False
+
+
+def test_remove_unavailable():
+    assert False
+
+
+def test_get_unavailable():
+    assert False
+
+
+def test_unavailable():
+    assert False
+
+
+def test_print_description():
+    assert False
