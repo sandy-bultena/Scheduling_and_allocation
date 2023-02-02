f052a42cdb8ee5ed147efd5f840de69d645897fa
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-02-02 08:27:44 -0500

    Backend: Began converting Course.
    Expanded constructor, added properties for name, needs_allocation and semester.

--- schedule/Schedule/Course.py
+++ schedule/Schedule/Course.py
@@ -1,3 +1,7 @@
+import re
+from warnings import warn
+
+
 # SYNOPSIS
 
 #    use Schedule::Course;
@@ -26,5 +30,69 @@ class Course(metaclass=CourseMeta):
     # ideally iterable is implemented with dict; if not, yaml write needs to be modified accordingly
     _max_id = 0
 
-    def __init__(self, **kwargs):
+    # -------------------------------------------------------------------
+    # new
+    #--------------------------------------------------------------------
+    def __init__(self, number, name: str = "", semester: str = "", **kwargs):
         self.name = "C"  # temp assignment to avoid crashes in Block __str__
+        Course._max_id += 1
+        self.__id = Course._max_id
+        self.number = number
+        self.name = name
+        self.semester = semester
+
+    # =================================================================
+    # id
+    # =================================================================
+    @property
+    def id(self):
+        """Returns the unique ID for this Course object."""
+        return self.__id
+
+    # =================================================================
+    # name
+    # =================================================================
+    @property
+    def name(self):
+        """Gets or sets the Course name."""
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str):
+        self.__name = new_name
+
+    # =================================================================
+    # needs allocation
+    # =================================================================
+    @property
+    def needs_allocation(self) -> bool:
+        """Does this course need to be allocated (i.e., to have a teacher assigned)?
+        
+        For example, Math and Human Relations do not need to be allocated to one of our teachers.
+        
+        Defaults to true."""
+        if not hasattr(self, '_allocation'):
+            self._allocation = True
+        return self._allocation
+
+    @needs_allocation.setter
+    def needs_allocation(self, allocation: bool):
+        self._allocation = allocation
+
+    # =================================================================
+    # semester
+    # =================================================================
+    @property
+    def semester(self):
+        """Gets or sets the Course's semester (accepted values: 'summer', 'winter', 'fall', or blank)."""
+        return self.__semester
+
+    @semester.setter
+    def semester(self, semester: str):
+        semester = semester.lower()
+        if not re.match("^(summer|winter|fall)"):
+            warn(f"invalid semester for course; {semester}", Warning, stacklevel=2)
+            semester = ""
+        self.__semester = semester
+    
+    
