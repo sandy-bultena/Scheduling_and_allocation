0318a38feafd7748c528533e88dd7d9a84696982
jacoblevy2002 <jacoblevy2002@gmail.com>
2023-02-23 17:46:54 -0500

    removed missed semester arguments

--- schedule/Schedule/unit_tests/test_Stream.py
+++ schedule/Schedule/unit_tests/test_Stream.py
@@ -92,7 +92,7 @@ def test_confirm_can_get_by_id():
 
 def test_share_blocks_finds_shared():
     """Confirm that share_blocks finds streams with both blocks"""
-    c = Course(semester = "summer")
+    c = Course()
     b1 = b2 = Block('Mon', '13:00', 2, 1)
     se = Section(course = c, schedule_id = 1)
     s = Stream()
@@ -103,7 +103,7 @@ def test_share_blocks_finds_shared():
 
 def test_share_blocks_ignores_non_shared():
     """Confirm that share_blocks ignores streams without both blocks"""
-    c = Course(semester = "summer")
+    c = Course()
     b1 = b2 = Block('Mon', '13:00', 2, 1)
     se = Section(course = c, schedule_id = 1)
     s = Stream()
