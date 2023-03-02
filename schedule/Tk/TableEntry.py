a3c79fd9df97e80615ff18c3c12985c86de376e4
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-02 09:12:41 -0500

    TK: Slowly beginning to understand how this conversion must unfold.

--- schedule/Tk/TableEntry.py
+++ schedule/Tk/TableEntry.py
@@ -8,5 +8,28 @@ from tkinter import ttk
 # ===================================================================
 
 class TableEntry(Frame):
-    def __init__(self):
-        pass
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    # ===================================================================
+    # populate
+    # ===================================================================
+    def populate(self, *args):
+        pane = Frame(self, name='TableEntry', border=2, relief='flat')
+        pane.pack(side='left', fill='both')
+
+        self.pane = pane
+        x_scrollbar = Scrollbar(self.pane, elementborderwidth=2,
+                                relief='ridge',
+                                width=12,
+                                orient=HORIZONTAL)
+        y_scrollbar = Scrollbar(self.pane, elementborderwidth=2,
+                                relief='ridge',
+                                width=12,
+                                orient=VERTICAL)
+
+        # ---------------------------------------------------------------
+        # create defaults here, because ConfigSpecs doesn't set
+        # those specifications until later, but we need this stuff
+        # now
+        # ---------------------------------------------------------------
