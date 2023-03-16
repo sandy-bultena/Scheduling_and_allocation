f03d0b24e1772cb717ced3fd67493c11d4c709b5
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-16 08:57:32 -0400

    CodeReview: Wrote unit test to confirm that app can create a LabUnavailableTime object.

---
+++ schedule/Schedule/unit_tests/test_LabUnavailableTime.py
@@ -0,0 +1,57 @@
+import pytest
+import sys
+from os import path
+from pony.orm import *
+
+sys.path.append(path.dirname(path.dirname(__file__)))
+
+from Schedule import Schedule
+from LabUnavailableTime import LabUnavailableTime
+from Lab import Lab
+from database.PonyDatabaseConnection import define_database, Scenario as dbScenario, \
+    Schedule as dbSchedule, Lab as dbLab
+from unit_tests.db_constants import *
+from ScheduleEnums import WeekDay
+
+db: Database
+s: dbSchedule
+
+@pytest.fixture(scope="module", autouse=True)
+def before_and_after_module():
+    global db
+    db = define_database(host=HOST, passwd=PASSWD, db=DB_NAME, provider=PROVIDER, user=USERNAME)
+    yield
+    db.drop_all_tables(with_all_data=True)
+    db.disconnect()
+    db.provider = db.schema = None
+
+
+@db_session
+def init_scenario_and_schedule():
+    global s
+    sc = dbScenario()
+    flush()
+    s = dbSchedule(semester="Winter 2023", official=False, scenario_id=sc.id, description="W23")
+
+
+@pytest.fixture(autouse=True)
+def run_before():
+    db.create_tables()
+    init_scenario_and_schedule()
+    Schedule.reset_local()
+    yield
+    db.drop_all_tables(with_all_data=True)
+
+
+@db_session
+def test_constructor_no_db():
+    """Verifies that the initializer can create a LabUnavailableTime object with
+    no corresponding database record."""
+    sched = Schedule.read_DB(1)
+    day = "mon"
+    start = "8:30"
+    dur = 2
+    num = 1
+    unavailable = LabUnavailableTime(day, start, dur, id=1, schedule=sched)
+    assert unavailable.day == day and unavailable.start == start and unavailable.duration == dur \
+           and unavailable.schedule == sched
