acc9e9a698d6a5e3bcb3b3e604c70bf5d4b738a0
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-21 16:16:00 -0400

    GUI: Added a main section to ApplicationEntry.py.

--- schedule/ApplicationEntry.py
+++ schedule/ApplicationEntry.py
@@ -45,21 +45,22 @@ def display_error_message(msg: str):
     error_window.mainloop()
 
 
-root = Tk()
-root.title("Scheduling Application -  Login")
-frm = ttk.Frame(root, padding=30)
-frm.grid()
-ttk.Label(frm, text="Please login to access the scheduler").grid(column=1, row=0)
-ttk.Label(frm, text="Username").grid(column=0, row=1)
-usernameInput = StringVar()
-ttk.Entry(frm, textvariable=usernameInput).grid(column=1, row=1, columnspan=2)
-ttk.Label(frm, text="Password").grid(column=0, row=2)
-passwdInput = StringVar()
-ttk.Entry(frm, textvariable=passwdInput, show="*").grid(column=1, row=2, columnspan=2)
-ttk.Button(frm, text="Login",
-           command=partial(verify_login, username=usernameInput, passwd=passwdInput))\
-    .grid(column=1, row=3, columnspan=1)
-statusString = StringVar()
-ttk.Label(frm, textvariable=statusString).grid(column=0, row=4, columnspan=2)
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Scheduling Application -  Login")
+    frm = ttk.Frame(root, padding=30)
+    frm.grid()
+    ttk.Label(frm, text="Please login to access the scheduler").grid(column=1, row=0)
+    ttk.Label(frm, text="Username").grid(column=0, row=1)
+    usernameInput = StringVar()
+    ttk.Entry(frm, textvariable=usernameInput).grid(column=1, row=1, columnspan=2)
+    ttk.Label(frm, text="Password").grid(column=0, row=2)
+    passwdInput = StringVar()
+    ttk.Entry(frm, textvariable=passwdInput, show="*").grid(column=1, row=2, columnspan=2)
+    ttk.Button(frm, text="Login",
+               command=partial(verify_login, username=usernameInput, passwd=passwdInput)) \
+        .grid(column=1, row=3, columnspan=1)
+    statusString = StringVar()
+    ttk.Label(frm, textvariable=statusString).grid(column=0, row=4, columnspan=2)
 
 root.mainloop()
