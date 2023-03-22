32b7208430030bfbaf6cbbdb884e9ad1cc2aeff4
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-22 10:18:38 -0400

    GUI: Added a new window to ApplicationEntry.py which lets user select an existing scenario from db or create a new one.

--- schedule/ApplicationEntry.py
+++ schedule/ApplicationEntry.py
@@ -4,16 +4,47 @@ from tkinter.ttk import Progressbar
 from functools import partial
 
 import mysql.connector
+from pony.orm import Database, db_session, commit
 
 from Schedule.unit_tests.db_constants import DB_NAME, HOST, PROVIDER
 import Schedule.database.PonyDatabaseConnection as PonyDatabaseConnection
 
 
-def verify_login(**kwargs: StringVar):
+@db_session
+def _display_scenario_selector(db: Database):
+    """Displays a window where the user can select an existing Scenario from the database,
+    or create a new one."""
+    scenario_window = Toplevel(root)
+    scenario_window.title = "SELECT SCENARIO"
+    scenario_window.grab_set()
+    scen_frm = ttk.Frame(scenario_window, padding=40)
+    scen_frm.grid()
+    ttk.Label(scen_frm, text="Select a scenario:").grid(row=0, column=0, columnspan=2)
+    db_scenarios = PonyDatabaseConnection.Scenario.select()
+    commit()
+    scen_list: list[PonyDatabaseConnection.Scenario] = [s for s in db_scenarios]
+    scenario_var = StringVar(value=scen_list)
+    l_box = Listbox(scen_frm, listvariable=scenario_var)
+    l_box.grid(row=0, column=0, columnspan=2)
+    ttk.Button(scen_frm, text="New", command=create_scenario).grid(row=2, column=0)
+    ttk.Button(scen_frm, text="Select").grid(row=2, column=1)
+    ttk.Button(scen_frm, text="Delete").grid(row=3, column=0)
+    ttk.Button(scen_frm, text="Cancel", command=scenario_window.destroy).grid(row=3, column=1)
+    scenario_window.mainloop()
+    pass
+
+
+@db_session
+def create_scenario():
+    """Create a new database Scenario object."""
+    # TODO: Refactor this later once changes have been merged from the code_review branch.
+    scenario = PonyDatabaseConnection.Scenario(name="Test", description="This is a test.",
+                                               year=2023)
+    commit()
+
+
+def _verify_login(**kwargs: StringVar):
     # Do something to verify the user's credentials.
-    # print(kwargs)
-    # for arg in kwargs:
-    #     print(kwargs[arg])
     username = kwargs['username'].get()
     passwd = kwargs['passwd'].get()
     if username is None or len(username) == 0 or passwd is None or len(passwd) == 0:
@@ -37,6 +68,7 @@ def verify_login(**kwargs: StringVar):
             pb.destroy()
             print(success_msg)
             statusString.set(success_msg)
+            _display_scenario_selector(db)
         except mysql.connector.DatabaseError as err:
             # Display a relevant error message for anything else that might go wrong with the
             # connection.
@@ -74,7 +106,7 @@ if __name__ == "__main__":
     passwdInput = StringVar()
     ttk.Entry(frm, textvariable=passwdInput, show="*").grid(column=1, row=2, columnspan=2)
     ttk.Button(frm, text="Login",
-               command=partial(verify_login, username=usernameInput, passwd=passwdInput)) \
+               command=partial(_verify_login, username=usernameInput, passwd=passwdInput)) \
         .grid(column=1, row=3, columnspan=1)
     statusString = StringVar()
     ttk.Label(frm, textvariable=statusString).grid(column=0, row=4, columnspan=2)
