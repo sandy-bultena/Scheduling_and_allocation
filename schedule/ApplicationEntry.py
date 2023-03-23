f650f6211db82e48e8eefe0e13e9554e93682870
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-23 15:26:51 -0400

    GUI: Beginning to extract scenario selection functionality into its own class.

--- schedule/ApplicationEntry.py
+++ schedule/ApplicationEntry.py
@@ -73,13 +73,66 @@ class LoginWindow:
             # hanging around as a distraction.
             self.parent.withdraw()
             # Then display the scenario selector window.
-            _display_scenario_selector(db)
+            ScenarioSelector(self.parent, db)
         except mysql.connector.DatabaseError as err:
             display_error_message(str(err))
         except TypeError as err:
             display_error_message(str(err))
         except mysql.connector.InterfaceError as err:
             display_error_message(str(err))
+
+
+class ScenarioSelector:
+    """Class representing a window which allows the user to pick from one of several scenarios.
+
+    Scenarios are retrieved from the database. If no scenarios exist, the selector gives the user
+    the functionality to create new ones. """
+
+    def __init__(self, parent: Tk, db: Database):
+        self.db = db
+        self.scen_dict = {}
+        self.window = self._setup_window(parent)
+        self.frame = self._setup_frame()
+        self._setup_interface()
+        self.window.mainloop()
+
+    def _setup_frame(self) -> Frame:
+        frame = ttk.Frame(self.window, padding=40)
+        frame.grid()
+        return frame
+
+    @staticmethod
+    def _setup_window(parent) -> Toplevel:
+        window = Toplevel(parent)
+        window.title("SELECT SCENARIO")
+        return window
+
+    def _setup_interface(self):
+        ttk.Label(self.frame, text="Select a scenario:").grid(row=0, column=0, columnspan=2)
+        self._get_all_scenarios()
+        self.scenario_var = StringVar(value=self.scen_list)
+        self.listbox = Listbox(self.frame, listvariable=self.scenario_var)
+        self.listbox.grid(row=1, column=0, columnspan=2)
+        ttk.Button(self.frame, text="New", command=self.add_new_scenario).grid(row=2, column=0)
+        ttk.Button(self.frame, text="Open").grid(row=2, column=1)
+        self.window.protocol("WM_DELETE_WINDOW", root.destroy)
+        pass
+
+    @db_session
+    def _get_all_scenarios(self):
+        """Retrieves all scenario records from the database, storing them within this
+        ScenarioSelector object."""
+        db_scenarios = PonyDatabaseConnection.Scenario.select()
+        flush()
+        self.scen_list = [s for s in db_scenarios]
+        for scen in self.scen_list:
+            self.scen_dict[str(scen)] = scen
+
+    @db_session
+    def add_new_scenario(self):
+        """Opens a window in which the user can add a new scenario to the database by filling out
+        a form."""
+        pass
 
 
 def check_num(newval):
