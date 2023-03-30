0e0bb687927dd7776b1bd9e49f87203430772816
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-30 09:30:13 -0400

    Backend: Added example of how DrawView should be used to module.

--- schedule/Export/DrawView.py
+++ schedule/Export/DrawView.py
@@ -4,53 +4,71 @@ from tkinter import Canvas
 
 from schedule.PerlLib import Colour
 from schedule.Schedule.Block import Block
-
-"""
-=head1 SYNOPSIS
-
-    use Schedule::Schedule;
-    use Tk;
-    use Export::PDF;
+"""SYNOPSIS
     
-    my $Schedule = Schedule->read_YAML('myschedule_file.yaml');
-    my $teacher  = $Schedule->teachers()->get_by_name("Sandy","Bultena");
-    my @blocks   = $schedule->blocks_for_teacher($teacher);
-        
-    # ----------------------------------------------------------
-    # create a pdf, as well as a Tk canvas
-    # ----------------------------------------------------------
-    my $pdf         = PDF->new();
-    my $mw          = MainWindow->new();
-    my $cn          = $mw->Canvas()->pack();
-
-    # ----------------------------------------------------------
-    # what scale you want
-    # ----------------------------------------------------------
-    my $scl = {
-             -xoff  => 1,       # before being scaled by xscl
-             -yoff  => 1,       # before being scaled by yscl
-             -xorg  => 0,       # start drawing at this position
-             -yorg  => 0,       # start drawing at this position
-             -xscl  => 100,     # stretch horizontally
-             -yscl  => 60,      # stretch vertically
-             -scale => 1,       # 1 = 100%.  Text may be modified if scale < 1
-    };
-
-    # ----------------------------------------------------------
-    # Draw the grid on both pdf and canvas
-    # ----------------------------------------------------------
-    DrawView->draw_background($cn,$scl);
-    DrawView->draw_background($pdf,$scl); 
+    from tkinter import *
+    from tkinter import ttk
     
+    from schedule.Export import DrawView
+    from schedule.Schedule.Teacher import Teacher
+    from schedule.Schedule.Block import Block
+    from schedule.Schedule.ScheduleEnums import WeekDay
+    from schedule.Schedule.database import PonyDatabaseConnection
+    from schedule.Schedule.database.db_constants import *
+
     # ----------------------------------------------------------
-    # Draw the teacher blocks on both pdf and canvas
+    # connect to the database, instantiate a Teacher and some Blocks.
     # ----------------------------------------------------------
-    foreach my $block (@blocks) {
-        DrawView->draw_block($cn,$block,$scl,"teacher");
-        DrawView->draw_block($pdf,$block,$scl,"teacher");
+    db = PonyDatabaseConnection.define_database(host=HOST, db=DB_NAME, user=USERNAME,
+                                                        passwd=PASSWD, provider=PROVIDER)
+
+    teacher = Teacher("John", "Smith")
+    block_1 = Block(WeekDay.Monday, "8:30", 1.5, 1)
+    block_2 = Block(WeekDay.Wednesday, "8:30", 1.5, 1)
+    block_1.assign_teacher(teacher)
+    block_2.assign_teacher(teacher)
+
+    blocks = [block_1, block_2]
+
+    # ----------------------------------------------------------
+    # Create the Tk main window and the canvas
+    # ----------------------------------------------------------
+    main_window = Tk()
+    # main_window.geometry('500x600-300-40')
+    cn = Canvas(main_window)
+    cn.pack(fill=BOTH)
+
+    # ----------------------------------------------------------
+    # Define what scale you want
+    # ----------------------------------------------------------
+    scl = {
+        'xoff': 1,
+        'yoff': 1,
+        'xorg': 0,
+        'yorg': 0,
+        'xscl': 100,
+        'yscl': 60,
+        'scale': 1
     }
 
+    # ----------------------------------------------------------
+    # draw the grid on the canvas.
+    # ----------------------------------------------------------
+    DrawView.draw_background(cn, scl)
+
+    # ----------------------------------------------------------
+    # Draw the teacher blocks on canvas. 
+    # Set the type argument to "lab" or "teacher" to change the
+    # colours and the information that is displayed in the 
+    # block.
+    # ----------------------------------------------------------
+    for block in blocks:
+        DrawView.draw_block(cn, block, scl, "stream")
+    main_window.mainloop()
+
 """
+
+
 
 # TODO: Make this into a static class?
 
@@ -494,4 +512,9 @@ B<Returns>
 A canvas CreateLine object
 
 
+"""
+
+
+"""
+Adapted to Python by Evan Laverdiere
 """
