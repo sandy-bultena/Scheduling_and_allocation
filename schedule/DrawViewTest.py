b58c540001b5fd61911a8be26e472c82ebe69bed
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-03-30 09:30:13 -0400

    Backend: Added example of how DrawView should be used to module.

--- schedule/DrawViewTest.py
+++ schedule/DrawViewTest.py
@@ -10,6 +10,9 @@ from schedule.Schedule.database.db_constants import *
 
 
 def main():
+    # ----------------------------------------------------------
+    # connect to the database, instantiate a Teacher and some Blocks.
+    # ----------------------------------------------------------
     db = PonyDatabaseConnection.define_database(host=HOST, db=DB_NAME, user=USERNAME,
                                                         passwd=PASSWD, provider=PROVIDER)
 
@@ -21,11 +24,17 @@ def main():
 
     blocks = [block_1, block_2]
 
+    # ----------------------------------------------------------
+    # Create the Tk main window and the canvas
+    # ----------------------------------------------------------
     main_window = Tk()
     # main_window.geometry('500x600-300-40')
     cn = Canvas(main_window)
     cn.pack(fill=BOTH)
 
+    # ----------------------------------------------------------
+    # Define what scale you want
+    # ----------------------------------------------------------
     scl = {
         'xoff': 1,
         'yoff': 1,
@@ -36,8 +45,14 @@ def main():
         'scale': 1
     }
 
+    # ----------------------------------------------------------
+    # draw the grid on the canvas.
+    # ----------------------------------------------------------
     DrawView.draw_background(cn, scl)
 
+    # ----------------------------------------------------------
+    # Draw the teacher blocks on canvas.
+    # ----------------------------------------------------------
     for block in blocks:
         DrawView.draw_block(cn, block, scl, "stream")
     main_window.mainloop()
