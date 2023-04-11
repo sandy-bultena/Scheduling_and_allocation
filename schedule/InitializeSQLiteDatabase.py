067cb04f74bd185b24cfa81736c8b07d8eb784e7
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-04-11 11:35:34 -0400

    General: Expanding script to initialize the SQLite database.

--- schedule/InitializeSQLiteDatabase.py
+++ schedule/InitializeSQLiteDatabase.py
@@ -1,11 +1,12 @@
 """Initializes an SQLite database file containing test data for the whole application."""
 from pony.orm import db_session, commit
 
+from schedule.Schedule.Course import Course
+from schedule.Schedule.Schedule import Schedule
 from schedule.Schedule.database import PonyDatabaseConnection
 from schedule.Schedule.database.db_constants import *
 
 
-@db_session
 def create_scenario() -> PonyDatabaseConnection.Scenario:
     if PonyDatabaseConnection.Scenario.get(id=1) is None:
         db_scenario = PonyDatabaseConnection.Scenario(name="Test Scenario",
@@ -17,7 +18,6 @@ def create_scenario() -> PonyDatabaseConnection.Scenario:
     return db_scenario
 
 
-@db_session
 def create_schedule(db_scen: PonyDatabaseConnection.Scenario) -> PonyDatabaseConnection.Schedule:
     if PonyDatabaseConnection.Schedule.get(id=1) is None:
         db_schedule = PonyDatabaseConnection.Schedule(
@@ -31,13 +31,32 @@ def create_schedule(db_scen: PonyDatabaseConnection.Scenario) -> PonyDatabaseCon
     return db_schedule
 
 
+def create_course() -> Course:
+    if PonyDatabaseConnection.Course.get(id=1) is None:
+        my_course = Course("420-101", "Intro to Programming", 1)
+    else:
+        db_course = PonyDatabaseConnection.Course.get(id=1)
+        commit()
+        my_course = Course(db_course.number, db_course.name, db_course.semester,
+                           db_course.allocation, id=db_course.id)
+    return my_course
+
+
 def main():
     if PROVIDER != "sqlite":
         raise ValueError(f"Invalid pony provider format retrieved from .env file: {PROVIDER}")
     db = PonyDatabaseConnection.define_database(provider=PROVIDER, filename=DB_NAME,
                                                 create_db=CREATE_DB)
-    db_scen = create_scenario()
-    db_sched = create_schedule(db_scen)
+    with db_session:
+        db_scen = create_scenario()
+        db_sched = create_schedule(db_scen)
+        my_sched = Schedule.read_DB(db_sched.id)
+
+    print(my_sched)
+
+    with db_session:
+        course_a = create_course()
+    print(course_a)
 
 
 if __name__ == "__main__":
