954408af258d5f3f5c35bc9b3dcd334b7c0839c6
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-04-13 08:10:01 -0400

    Presenter: Began implementing Scheduler.py.

---
+++ schedule/Presentation/Scheduler.py
@@ -0,0 +1,38 @@
+class Scheduler:
+    """
+    # ==================================================================
+    # This is the main entry point for the Scheduler Program
+    # ==================================================================
+
+    # uses MVP protocol, so the GUI must be implement the methods, etc
+    # defined in SchedulerManagerViewInterface.pm
+
+
+    """
+    # ==================================================================
+    # global vars
+    # ==================================================================
+    # NOTE: Some of these class variables are probably unnecessary now.
+    user_base_dir = None
+    preferences = {}
+    schedule = None
+    current_schedule_file = ""
+    current_directory = ""
+    file_types = (("Schedules", ".yaml"), ("All Files", "*"))
+    gui = None
+    views_manager = None
+
+    # ==================================================================
+    # required Notebook pages
+    # ==================================================================
+    # NOTE: Come back to these later.
+    required_pages = []
+
+    pages_lookup = dict()
+
+    # ==================================================================
+    # main
+    # ==================================================================
+    @staticmethod
+    def main():
+        pass
