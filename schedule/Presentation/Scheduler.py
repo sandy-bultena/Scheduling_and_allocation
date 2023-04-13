fae461e48390abf3cedc39d48bf9eb724a3ffb18
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2023-04-13 11:10:24 -0400

    GUI/Presenter: Partial implementation of several Scheduler.py methods.

--- schedule/Presentation/Scheduler.py
+++ schedule/Presentation/Scheduler.py
@@ -1,4 +1,7 @@
+import pony.orm
+
 from ..GUI.SchedulerTk import SchedulerTk
+from ..Schedule.Schedule import Schedule
 from ..UsefulClasses.NoteBookPageInfo import NoteBookPageInfo
 
 """
@@ -151,3 +154,90 @@ def menu_info():
 
     return buttons, actions, menu
 
+
+# ==================================================================
+# new_schedule
+# ==================================================================
+def new_schedule():
+    # NOTE: Sandy left this to-do in the original Perl script: TODO: save previous schedule?
+    global schedule, current_schedule_file
+    schedule = Schedule()  # TODO: Change this to reflect what's in the db.
+    current_schedule_file = None
+
+    _schedule_file_changed(None)
+
+
+# ==================================================================
+# open_schedule
+# ==================================================================
+def open_schedule():
+    # This is another one that will have to change.
+    pass
+
+
+# ==================================================================
+# schedule file has changed
+# ==================================================================
+def _schedule_file_changed(file):
+    # Another one with lots of changes to be made.
+    global schedule
+    if file and schedule:
+        global current_schedule_file, current_directory
+        current_schedule_file = file
+        current_directory = file
+        write_ini()
+
+
+# ==================================================================
+# save (as) schedule
+# ==================================================================
+def save_schedule():
+    _save_schedule(False)
+
+
+def save_as_schedule():
+    _save_schedule(True)
+
+
+def _save_schedule(save_as: bool):
+    global schedule, gui
+    gui: SchedulerTk
+    schedule: Schedule
+
+    if schedule is None:
+        gui.show_error("Save Schedule", "There is no schedule to save!")
+        return
+
+    # get file to save to
+    file: str
+    global current_schedule_file
+    if save_as or not current_schedule_file:
+        file = gui.choose_file()
+        # NOTE: This probably doesn't need to be implemented anymore.
+        if not file:
+            return
+    else:
+        file = current_schedule_file
+
+    # Save to database.
+    try:
+        schedule.write_DB()
+    except pony.orm.Error as err:
+        gui.show_error("Save Schedule", f"Cannot save schedule:\nError: {err}")
+
+    # save the current file for later use.
+    # NOTE: We probably don't need these anymore.
+
+    return
+
+
+# ==================================================================
+# save as CSV
+# ==================================================================
+def save_as_csv():
+    pass
+
+# ==================================================================
+# update_choices_of_schedulable_views
+# (what teachers/labs/streams) can we create schedules for?
+# ==================================================================
