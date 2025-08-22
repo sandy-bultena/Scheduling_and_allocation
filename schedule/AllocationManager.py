214e42e52f94c4f55645f81305a49e018826c60c
sandy-bultena <sandy.bultena@sympatico.ca>
2025-08-22 11:47:46 -0400

    Working on old Windows machine

--- schedule/AllocationManager.py
+++ schedule/AllocationManager.py
@@ -5,9 +5,17 @@ bin_dir: str = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(bin_dir, "../"))
 
 from presenter.allocation_manager import AllocationManager
+a=input("press enter to continue")
+print(sys.version)
+print(sys.version_info)
 
 def main():
-    AllocationManager(bin_dir=bin_dir)
+    a=input("press enter to continue")
+    try:
+        AllocationManager(bin_dir=bin_dir)
+    except Exception as e:
+        print(e)
+    a=input("press enter to continue")
 
 
 if __name__ == "__main__":
