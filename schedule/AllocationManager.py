652aa1c4832efe3daf34473c74141aefadeea8fb
Sandy Bultena <sandy.bultena@sympatico.ca>
2025-08-23 10:28:48 -0400

    Moving files around, and protecting main code from crashing without warnings

--- schedule/AllocationManager.py
+++ schedule/AllocationManager.py
@@ -4,18 +4,26 @@ import sys
 bin_dir: str = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(bin_dir, "../"))
 
-from presenter.allocation_manager import AllocationManager
-a=input("press enter to continue")
-print(sys.version)
-print(sys.version_info)
 
 def main():
-    a=input("press enter to continue")
     try:
+        from presenter.allocation_manager import AllocationManager
         AllocationManager(bin_dir=bin_dir)
     except Exception as e:
+        print()
+        print("==================================================================")
+        print("ERROR:")
+        print("==================================================================")
         print(e)
-    a=input("press enter to continue")
+        print()
+        print("==================================================================")
+        print("System info")
+        print("==================================================================")
+        print(sys.version)
+        print(sys.version_info)
+        print()
+        print()
+        _=input("press enter to continue")
 
 
 if __name__ == "__main__":
--- schedule/SchedulerProgram.py
+++ schedule/SchedulerProgram.py
@@ -4,10 +4,26 @@ import sys
 bin_dir: str = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(bin_dir, "../"))
 
-from presenter.scheduler import Scheduler
-
 def main():
-    Scheduler(bin_dir=bin_dir)
+    try:
+        from presenter.scheduler import Scheduler
+        Scheduler(bin_dir=bin_dir)
+    except Exception as e:
+        print()
+        print("==================================================================")
+        print("ERROR:")
+        print("==================================================================")
+        print(e)
+        print()
+        print("==================================================================")
+        print("System info")
+        print("==================================================================")
+        print(sys.version)
+        print(sys.version_info)
+        print()
+        print()
+        _=input("press enter to continue")
+
 
 
 if __name__ == "__main__":
