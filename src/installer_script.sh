279b3f7168e9a6e01259d13da1c60d720a77fe16
Sandy Bultena <sandy.bultena@sympatico.ca>
2025-09-02 12:11:00 -0400

    ready for release (already uploaded to pypi)

--- src/installer_script.sh
+++ src/installer_script.sh
@@ -1,4 +1,17 @@
 # =================================================================================================================
+# to create scripts that can be downloaded via pip install - requires a python installation
+# =================================================================================================================
+# NOTE: if you don't already have the password, ask the maintainer of this code for it
+#
+# Change the version number in pyproject.toml
+# install 'build' and twine if you don't already have them
+# from the Scheduling_and_allocation-PythonWithoutDB directory (i.e. from the directory where the .toml file is
+
+python3 -m build
+python3 twine upload dist/*
+
+
+# =================================================================================================================
 # to create an executable folder that needs no python installer
 # =================================================================================================================
 # NOTE: this is OS dependent, so must be done for each OS that you wish to have an installer for
@@ -9,4 +22,4 @@ python -m pip install pyinstaller  # or python3 if on mac
 
 python -m PyInstaller SchedulerProgram.py   --icon scheduler_icon.ico   --add-binary scheduling_and_allocation/schedule_logo.png:scheduling_and_allocation --add-binary scheduling_and_allocation/schedule_ico.png:scheduling_and_allocation --add-binary scheduling_and_allocation/modified_tk/Images/:scheduling_and_allocation/modified_tk/Images/ --add-data scheduling_and_allocation/export/view_template.tex:scheduling_and_allocation/export/ --add-binary scheduling_and_allocation/allocation_ico.png:scheduling_and_allocation/ --add-binary scheduling_and_allocation/allocation_logo.png:scheduling_and_allocation/
 
-python3 -m PyInstaller AllocationManager.py --icon allocation_icon.ico  --add-binary scheduling_and_allocation/schedule_logo.png:scheduling_and_allocation --add-binary scheduling_and_allocation/schedule_ico.png:scheduling_and_allocation --add-binary scheduling_and_allocation/modified_tk/Images/:scheduling_and_allocation/modified_tk/Images/ --add-data scheduling_and_allocation/export/view_template.tex:scheduling_and_allocation/export/ --add-binary scheduling_and_allocation/allocation_ico.png:scheduling_and_allocation/ --add-binary scheduling_and_allocation/allocation_logo.png:scheduling_and_allocation/
+python -m PyInstaller AllocationManager.py --icon allocation_icon.ico  --add-binary scheduling_and_allocation/schedule_logo.png:scheduling_and_allocation --add-binary scheduling_and_allocation/schedule_ico.png:scheduling_and_allocation --add-binary scheduling_and_allocation/modified_tk/Images/:scheduling_and_allocation/modified_tk/Images/ --add-data scheduling_and_allocation/export/view_template.tex:scheduling_and_allocation/export/ --add-binary scheduling_and_allocation/allocation_ico.png:scheduling_and_allocation/ --add-binary scheduling_and_allocation/allocation_logo.png:scheduling_and_allocation/
